137ab6cd9fe7881885a704f80fd0c82584a49e1d
liulinxin <liulinxin@kingsoft.com>
2024-08-30 18:45:49 +0800

    add：增加dubbo调用

---
+++ provider/src/main/java/org/licode/provider/api/DemoService.java
@@ -0,0 +1,10 @@
+package org.licode.provider.api;
+
+/**
+ * @author : by liulinxin
+ * @date : created 2024/8/30 17:19
+ * @description:
+ */
+
+public interface DemoService {
+}
---
+++ provider/src/main/java/org/licode/provider/service/DemoServiceImpl.java
@@ -0,0 +1,14 @@
+package org.licode.provider.service;
+
+import org.apache.dubbo.config.annotation.DubboService;
+import org.licode.provider.api.DemoService;
+
+/**
+ * @author : by liulinxin
+ * @date : created 2024/8/30 16:41
+ * @description:
+ */
+
+@DubboService
+public class DemoServiceImpl implements DemoService {
+}
